bd426fc176ca9f43b9979053193a7776a86ba484
Kitti <kittitoth8@gmail.com>
2019-08-31 22:38:21 +0200

    feat: create staff class

--- app.php
+++ app.php
@@ -1,22 +1,38 @@
 <?php
 include "config.php";
 
-// set response header
-header('Content-type: application/json');
-
-// get all necessary data from database
-$mysqlQuery = "SELECT first_name, last_name, gender, birth_date, hire_date, titles.title, departments.dept_name, salaries.salary FROM employees LEFT JOIN dept_emp on employees.emp_no = dept_emp.emp_no LEFT JOIN departments on dept_emp.dept_no = departments.dept_no LEFT JOIN titles on employees.emp_no = titles.emp_no LEFT JOIN salaries on employees.emp_no = salaries.emp_no";
-$query = mysqli_query($conn, $mysqlQuery);
-
-$res = [];
-
-// fetch database response as array
-while ($row = mysqli_fetch_array($query))
+class Staff
 {
-  $res[] = $row;
+  public $name;
+
+  function __construct ($inputName)
+  {
+    $this->name = $inputName;
+  }
+
+  public function sendData($connectionToDb)
+  {
+    // set response header
+    header('Content-type: application/json');
+
+    // get all necessary data from database
+    $mysqlQuery = "SELECT first_name, last_name, gender, birth_date, hire_date, titles.title, departments.dept_name, salaries.salary FROM employees LEFT JOIN dept_emp on employees.emp_no = dept_emp.emp_no LEFT JOIN departments on dept_emp.dept_no = departments.dept_no LEFT JOIN titles on employees.emp_no = titles.emp_no LEFT JOIN salaries on employees.emp_no = salaries.emp_no";
+    $query = mysqli_query($connectionToDb, $mysqlQuery);
+
+    $res = [];
+
+    // fetch database response as array
+    while ($row = mysqli_fetch_array($query))
+    {
+      $res[] = $row;
+    }
+
+    // send data to client
+    echo json_encode($res);
+  }
 }
 
-// send data to client
-echo json_encode($res);
+$testStaff = new Staff("test");
+$testStaff->sendData($conn);
 
 ?>
