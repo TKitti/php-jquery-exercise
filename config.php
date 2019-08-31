33fb96f2afd281c4aee6d1c2022b9d6045001b91
Kitti <kittitoth8@gmail.com>
2019-08-31 20:16:13 +0200

    fix: environment variables are noticed

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-require '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::create('/.env');
+$dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
 $host = getenv('DB_HOST');
