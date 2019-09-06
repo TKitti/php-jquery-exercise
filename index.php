3de043c49f5dbf0d98f0693dc1f84fcb078583a5
Kitti <kittitoth8@gmail.com>
2019-09-06 21:20:26 +0200

    feat: add jquery ui arrow icons

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Employees</title>
+  <link rel="stylesheet" href="assets/styles/style.css">
+  <link rel="stylesheet" href="assets/scripts/jquery/jquery-ui.min.css">
+  <script src="assets/scripts/jquery/jquery.js"></script>
+  <script src="assets/scripts/jquery/jquery-ui.min.js"></script>
 </head>
 <body>
   <div class="container">
@@ -12,20 +16,20 @@
       <thead>
         <tr>
           <th></th>
-          <th>Name</th>
+          <th>Name <span class="ui-icon ui-icon-triangle-1-n arrow-up"></span><span class="ui-icon ui-icon-triangle-1-s arrow-down"></span></th>
           <th>Gender</th>
-          <th>Birth date</th>
-          <th>Hire date</th>
-          <th>Title</th>
-          <th>Department</th>
+          <th>Birth date <span class="ui-icon ui-icon-triangle-1-n arrow-up"></span><span class="ui-icon ui-icon-triangle-1-s arrow-down"></span></th>
+          <th>Hire date <span class="ui-icon ui-icon-triangle-1-n arrow-up"></span><span class="ui-icon ui-icon-triangle-1-s arrow-down"></span></th>
+          <th>Title <span class="ui-icon ui-icon-triangle-1-n arrow-up"></span><span class="ui-icon ui-icon-triangle-1-s arrow-down"></span></th>
+          <th>Department <span class="ui-icon ui-icon-triangle-1-n arrow-up"></span><span class="ui-icon ui-icon-triangle-1-s arrow-down"></span></th>
           <th>Salary</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody class="table-body"></tbody>
     </table>
   </div>
 
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script src="./scripts/script.js"></script>
+  <script src="assets/scripts/script.js"></script>
 </body>
 </html>
